55872ea4398832e87b365c8bbe84e395634cb609
afiqnexea <afiqnrzm@hotmail.com>
2023-07-13 03:14:46 +0800

    test1

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hewo/i);
+  const linkElement = screen.getByText(/peeps/i);
   expect(linkElement).toBeInTheDocument();
 });
