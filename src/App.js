a624c0fb88a24a1caa375e096a05c58ee933c56a
afiqnexea <afiqnrzm@hotmail.com>
2023-07-12 21:28:21 +0800

    te

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 style={{ fontSize: "200px", color: "pink", textAlign: "center" }} >Hewo cutipie</h1>
+      <h1 style={{ fontSize: "200px", color: "pink", textAlign: "center" }}>Hewo cutipie</h1>
     </>
   );
 }
